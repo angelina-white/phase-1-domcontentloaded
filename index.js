7defa1ec63b209b992b3285666f75c284d59ea83
Angelina <angelinarwhite@gmail.com>
2022-04-30 17:33:23 -0400

    commit tree 9b0a2a53491604fa87e15b0391aa595b7c5f612f

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    const changeText = document.getElementById('text');
+    changeText.textContent = "This is really cool!";
+  });
+  
